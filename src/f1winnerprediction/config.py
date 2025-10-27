a0127d29fb914f9f4e6a394d5fca6fb78d8436cb
Dhia Ben Hamouda <dhiabenhamouda2002@gmail.com>
2025-10-27 21:52:41 +0100

    - Added Caching and checkpointing for data fetching - displaying basic information and first attempts at creating train/test data

---
+++ src/f1winnerprediction/config.py
@@ -0,0 +1,4 @@
+import pathlib
+from pathlib import Path
+CACHE_DIR: pathlib.PosixPath = Path("/media/dhiabenhamouda/Dhia/Work/F1WinnerPrediction/src/f1winnerprediction/cache")
+CHECKPOINT_DIR: pathlib.PosixPath = Path("/media/dhiabenhamouda/Dhia/Work/F1WinnerPrediction/src/f1winnerprediction/checkpoints")
