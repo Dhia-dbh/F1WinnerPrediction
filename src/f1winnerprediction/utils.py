14682d84fdef507f5541eb15ef80e5d8734d6b85
Dhia Ben Hamouda <dhiabenhamouda2002@gmail.com>
2025-11-04 19:44:21 +0100

    improved instance type checking

--- src/f1winnerprediction/utils.py
+++ src/f1winnerprediction/utils.py
@@ -2,13 +2,14 @@ import pandas as pd
 from pathlib import Path
 
 def write_to_file(output: pd.DataFrame | str):
-	if type(output) == pd.DataFrame:
+	if isinstance(output, pd.DataFrame):
 		output.to_csv("output.csv")
 		return
-	if type(output) == str:
+	if isinstance(output, str):
 		with open("output.txt", "w") as f:
 			f.write(output)
 			return
+	# TODO: THROW EXCEPTION
 	print(f"UNSUPPORTED OBJECT TYPE:  {type(output)}")
 
 
